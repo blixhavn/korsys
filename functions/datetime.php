61aa05a098fc82c940f342a5c5356b3a1b0ab8f4
Emil Taylor Bye <emil@wodinaz.com>
2013-11-25 14:37:46 +0100

    Fixed søndag

--- functions/datetime.php
+++ functions/datetime.php
@@ -2,44 +2,44 @@
 
 	function datetime_to_norstring($datetime){
 
-		$week = array('mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag', 'søndag');
+		$week = array('søndag', 'mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag');
 		$mon = array('januar','februar','mars','april','mai','juni','juli','august','september','oktober','november','desember');
 
 		$time = getdate(strtotime($datetime));
-		return $week[$time[wday]-1]." "
-			.$time[mday].". ".$mon[$time[mon]-1]." kl. ".$time[hours].":"
-			.str_pad($time[minutes]."", 2, "0",STR_PAD_LEFT);
+		return $week[$time['wday']]." "
+			.$time['mday'].". ".$mon[$time['mon']-1]." kl. ".$time['hours'].":"
+			.str_pad($time['minutes']."", 2, "0",STR_PAD_LEFT);
 	}
 
 	function datetime_to_norstring_stor($datetime){
 
-			$week = array('Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag');
+			$week = array('Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag');
 			$mon = array('januar','februar','mars','april','mai','juni','juli','august','september','oktober','november','desember');
 
 			$time = getdate(strtotime($datetime));
-			return $week[$time[wday]-1]." "
-				.$time[mday].". ".$mon[$time[mon]-1]." kl. ".$time[hours].":"
-				.str_pad($time[minutes]."", 2, "0",STR_PAD_LEFT);
+			return $week[$time['wday']]." "
+				.$time['mday'].". ".$mon[$time['mon']-1]." kl. ".$time['hours'].":"
+				.str_pad($time['minutes']."", 2, "0",STR_PAD_LEFT);
 	}
 	
 	function datetime_to_date($datetime){
 
-		$week = array('mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag', 'søndag');
+		$week = array('søndag', 'mandag', 'tirsdag', 'onsdag', 'torsdag', 'fredag', 'lørdag');
 		$mon = array('januar','februar','mars','april','mai','juni','juli','august','september','oktober','november','desember');
 
 		$time = getdate(strtotime($datetime));
-		return $week[$time[wday]-1]." "
-			.$time[mday].". ".$mon[$time[mon]-1];
+		return $week[$time['wday']]." "
+			.$time['mday'].". ".$mon[$time['mon']-1];
 	}
 
 	function datetime_to_date_stor($datetime){
 
-			$week = array('Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag');
+			$week = array('Søndag', 'Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag');
 			$mon = array('januar','februar','mars','april','mai','juni','juli','august','september','oktober','november','desember');
 
 			$time = getdate(strtotime($datetime));
-			return $week[$time[wday]-1]." "
-				.$time[mday].". ".$mon[$time[mon]-1];
+			return $week[$time['wday']]." "
+				.$time['mday'].". ".$mon[$time['mon']-1];
 	}
 	
 	function datetime_to_time($datetime) {
