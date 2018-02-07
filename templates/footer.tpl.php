7cc6b3dc4bdd3e52bf5d6aa4349191f2ac1538b9
Øystein Blixhavn <oystein@blixhavn.no>
2018-02-07 23:22:03 +0100

    Update import

--- templates/footer.tpl.php
+++ templates/footer.tpl.php
@@ -4,7 +4,7 @@
 
 <!-- Include JS at the bottom of the page -->
 <script src="assets/js/jquery-1.9.1.js"></script>
-<script src="assets/js/jquery-ui-1.10.2.custom.min.js"></script>
+<script src="assets/js/jquery-ui-1.10.2.min.js"></script>
 <script src="assets/js/jquery.formvalidator.js"></script>
 <script src="assets/js/jquery-ui-timepicker-addon.js"></script>
 
